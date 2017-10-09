876359062673755b7aa80f69731cee3403310657
liuzhen1025 <liuzhen_success@126.com>
2017-10-09 17:01:05 +0800

    Future 模式测试

--- GuavaTest/src/test/java/com/gennlife/guava/ListenableFutureTest.java
+++ GuavaTest/src/test/java/com/gennlife/guava/ListenableFutureTest.java
@@ -4,6 +4,7 @@ import com.google.common.util.concurrent.*;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 /**
@@ -15,16 +16,35 @@ public class ListenableFutureTest {
         ListeningExecutorService executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
         ListenableFuture<Integer> future = executorService.submit(new Callable<Integer>() {
             public Integer call() throws Exception {
+                Thread.sleep(5000);
                 return 1;
             }
         });
+
         Futures.addCallback(future, new FutureCallback<Integer>() {
             public void onSuccess(Integer integer) {
-                System.out.println(integer);
+                //System.out.println(integer);
             }
             public void onFailure(Throwable throwable) {
 
             }
         });
+        try {
+            System.out.println("deal something");
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+       /* while (true){
+            try {
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("1111111111111");
+        }*/
     }
 }
